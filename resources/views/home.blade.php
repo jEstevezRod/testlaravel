373b83365fcb94ef218bbd2ae53a06930439034a
Jesus Estevez <jesestrod1@gmail.com>
2021-02-01 01:29:59 +0100

    Little changes

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -26,7 +26,7 @@
         </div>
         <div class="row pt-default-header">
             <div class="col d-flex flex-column">
-                <h4 class="text-center feature-title text-white">{!! __('home.features_title') !!}</h4>
+                <p class="text-center feature-title text-white">{!! __('home.features_title') !!}</p>
                 <p class="text-center text-white h5 mt-4">
                     {!! __('home.features_text') !!}
                 </p>
